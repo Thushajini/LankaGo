2d8a022230bddfdafbe604424f0377648e142031
Thushajini <thushayoga1205@gmail.com>
2026-05-30 10:21:32 +0530

    setup react,vite,tailwindcss

---
+++ lankago/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="bg-green-500 h-screen flex items-center justify-center">
+    <h1 className="text-white text-4xl font-bold">
+       LanakaGo 🌴
+        </h1>
+    </div>
+  )
+}
+
+export default App
