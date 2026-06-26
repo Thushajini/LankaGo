233198926253616fe18869258caefc9b772a6655
Thushajini <thushayoga1205@gmail.com>
2026-06-26 18:03:42 +0530

    add photo and map

--- lankago/src/components/CategoryFilter.jsx
+++ lankago/src/components/CategoryFilter.jsx
@@ -5,18 +5,23 @@ export default function CategoryList({
   setSelectedCategory,
 }) {
   return (
-    <div className="px-5 mt-4">
-      <div className="flex gap-3 overflow-x-auto scrollbar-hide">
+    <div className="px-3 sm:px-5 mt-4">
+      
+      <div className=" flex gap-1 sm:gap-3  overflow-x-auto  pb-2 scrollbar-hide scroll-smooth
+      ">
+
         {categories.map((category) => (
           <button
             key={category}
             onClick={() => setSelectedCategory(category)}
-            className={`px-5 py-2 rounded-full whitespace-nowrap flex-shrink-0
+            className={`flex-shrink-0 whitespace-nowrap px-4 sm:px-5 py-1.5 sm:py-2 text-sm sm:text-base rounded-full transition-all duration-200
+
               ${
                 selectedCategory === category
-                  ? "bg-green-600 text-white"
-                  : "bg-gray-200 text-gray-700"
-              }`}
+                  ? "bg-green-600 text-white shadow-md"
+                  : "bg-gray-200 text-gray-700 hover:bg-gray-300"
+              }
+            `}
           >
             {category}
           </button>
---
+++ lankago/src/utils/imageMap.js
@@ -0,0 +1,15 @@
+import nallur from "../assets/images/nallur.jpg";
+import sigirija from "../assets/images/sigirija.jpg";
+import yala from "../assets/images/yala.jpg";
+import gallefort from "../assets/images/gallefort.jpg";
+import ella from "../assets/images/ella.jpg";
+import cave from "../assets/images/cave.jpg";
+
+export const imageMap ={
+    nallur,
+    sigirija,
+    yala,
+    gallefort,
+    ella,
+    cave,
+};
